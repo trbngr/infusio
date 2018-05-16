8ba386806040cbff720322198768baee53131ab5
Chris Martin <chris.martin@eventday.com>
2018-05-15 20:34:34 -0700

    fixup publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-root=./src/Infusio/bin/Debug/
+
+root=./src/Infusio/bin/Debug
 rm -rf $root
+dotnet clean
 dotnet pack
-for pkg in $root*.nupkg; do
-	dotnet nuget push ${pkg} -k ${NUGET_API_KEY} -s https://www.nuget.org
+for file in "${root}/infusio.*.nupkg"; do
+	dotnet nuget push $file -k $NUGET_API_KEY -s https://www.nuget.org
 done
